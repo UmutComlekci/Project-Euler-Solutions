4b8d13c6afe720210f51c9a36ebe95cce621a638
oguncukur <cukurogun@gmail.com>
2018-10-25 16:51:41 +0300

    Updated

--- dotnet/src/Problem6.cs
+++ dotnet/src/Problem6.cs
@@ -3,7 +3,7 @@ using System;
 namespace ProjectEuler
 {
     /*
-     Question: The sum of the squares of the first ten natural numbers is, 385
+    * Question: The sum of the squares of the first ten natural numbers is, 385
       The square of the sum of the first ten natural numbers is, 3025
       Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
       Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.  
@@ -11,16 +11,16 @@ namespace ProjectEuler
 
     public static class Problem6
     {
-        public static double SunSquareDifference()
+        public static double SumSquareDifference()
         {
-            double sum = 0;
-            double sum2 = 0;
+            double sumOfSquare = 0;
+            double squareOfSum = 0;
             for (int i = 0; i <= 100; i++)
             {
-                sum += Math.Pow(i, 2);
-                sum2 += i;
+                sumOfSquare += Math.Pow(i, 2);
+                squareOfSum += i;
             }
-            return (Math.Pow(sum2, 2) - sum);
+            return (Math.Pow(squareOfSum, 2) - sumOfSquare);
         }
     }    
 }
