5b9d5ba9c9ba06aa430eea05376e4c90dad718cc
Umut Çömlekçioğlu <umut-776@hotmail.com>
2018-10-25 11:17:19 +0300

    Problem1 question writed

--- dotnet/Problems/Problem1.cs
+++ dotnet/Problems/Problem1.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace ProjectEuler
 {
+    /*
+    * Question: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    * Find the sum of all the multiples of 3 or 5 below 1000.
+    */
+
     public static class Problem1
     {
         public static int SumOfDivision()
