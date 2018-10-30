8b573b8b069c6d3ab4b5f26a7f97d2e31d35fb38
ogulcanakkir <o.akkir59@gmail.com>
2018-10-30 10:00:18 +0300

    Problem7 has been solved

--- dotnet/src/Problem7.cs
+++ dotnet/src/Problem7.cs
@@ -6,41 +6,39 @@ namespace ProjectEuler
 {
     public static class Problem7
     {
-        public static int bulunan = 0, son = 10001;
-        public static List<int> asalsayilar = new List<int>();
+        public static int CatchPrimeNumber = 0, LastPrimeNumber = 10001;
+        public static List<int> PrimeNumbers = new List<int>();
 
-        public static void Problem7_Answer()
+        public static void Problem7Answer()
         {
-            int n = 2, son_asal_sayi = 0;
+            int n = 2, CaptureLastPrimeNumber = 0;
 
             do
             {
-                if (Asal_Mi(n))
+                if (IsItPrime(n))
                 {
-                    asalsayilar.Add(n);
-                    son_asal_sayi = n;
-                    bulunan++;
+                    PrimeNumbers.Add(n);
+                    CaptureLastPrimeNumber = n;
+                    CatchPrimeNumber++;
                 }
                 n++;
 
-            } while (bulunan < son);
+            } while (CatchPrimeNumber < LastPrimeNumber);
 
-            Console.WriteLine("10001. Asal sayi: " + son_asal_sayi);
+            Console.WriteLine("10001. Prime Number: " + CaptureLastPrimeNumber);
             Console.ReadKey();
         }
 
-        public static bool Asal_Mi(int n)
+        public static bool IsItPrime(int n)
         {
-
-            //Console.WriteLine(n);
-
-            foreach (int sayi in asalsayilar)
+            for(int i=2;i<=n;i++)
             {
-                if (sayi * sayi > n)
+                if(i==n)
                 {
                     break;
                 }
-                else if (n % sayi == 0)
+
+                if(n % i == 0)
                 {
                     return false;
                 }
