8208eb970dad54ad0181e4b403bd618cbe9fdeb6
erezgi <ez.erarslan@gmail.com>
2018-10-30 14:14:13 +0300

    .

--- dotnet/src/Problem9.cs
+++ dotnet/src/Problem9.cs
@@ -4,7 +4,7 @@ namespace ProjectEuler
 {
     /*
     * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-    * a2 + b2 = c2
+    * a^2 + b^2 = c^2
     * For example, 32 + 42 = 9 + 16 = 25 = 52.
     * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     * Find the product abc.
@@ -12,7 +12,7 @@ namespace ProjectEuler
 
     public static class Problem9
     {
-        public static string Pythagorean()
+        public static double Pythagorean()
         {
             int a=0,b=0,c=1000;
             for(a = 1; a<c; a++){
@@ -20,11 +20,11 @@ namespace ProjectEuler
                    c = 1000 - a - b;
                    double sumofsquares = Math.Pow(a,2) + Math.Pow(b,2);
                    if(sumofsquares == Math.Pow(c,2)){
-                       return $"{a}{b}{c}";
+                       return a*b*c;
                    }
                 }
             }
-            return "There is no way to do a Pythagorean with a + b + c = 1000.";
+            return 0;
         } 
     }    
 }
